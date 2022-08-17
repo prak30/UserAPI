8b8ab3f6dd3e0bbd233cce49ae381ee9cbdc5c8f
pranav <katkarpranav4@gmail.com>
2022-08-17 21:57:34 +0530

    Lombok configured

--- src/main/java/com/example/demo/dto/UserDto.java
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -1,27 +1,18 @@
 package com.example.demo.dto;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 @Setter
 @Getter
 @ToString
+@Data
 public class UserDto {
 	private String email;
 	private String password;
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+
 	
 	
 	
